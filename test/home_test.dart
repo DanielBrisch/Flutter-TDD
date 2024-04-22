8c810a3bbc2ff4546ac11d8e8c76cd74898d1e4d
daniel.cibolli <daniel.cibolli@viasoft.com.br>
2024-04-22 08:19:54 -0300

    start learing mockito

--- test/home_test.dart
+++ test/home_test.dart
@@ -1,3 +1,4 @@
+import 'package:estilizacao_componentes/components/box_card.dart';
 import 'package:estilizacao_componentes/data/bank_inherited.dart';
 import 'package:estilizacao_componentes/screens/home.dart';
 import 'package:flutter/material.dart';
@@ -20,5 +21,24 @@ void main() {
         .pumpWidget(MaterialApp(home: BankInherited(child: const Home())));
     expect(find.byKey(const Key('testKey')), findsOneWidget);
   });
-  testWidgets('finds five BoxCards', (tester) async {});
+  testWidgets('finds five BoxCards', (tester) async {
+    await tester
+        .pumpWidget(MaterialApp(home: BankInherited(child: const Home())));
+    expect(find.byWidgetPredicate((widget) {
+      if (widget is BoxCard) {
+        return true;
+      } else {
+        return false;
+      }
+    }), findsNWidgets(5));
+  });
+  testWidgets('When tap Deposit should upload earned in 10', (tester) async {
+    await tester
+        .pumpWidget(MaterialApp(home: BankInherited(child: const Home())));
+    await tester.tap(find.text('Deposit'));
+    await tester.tap(find.text('Earned'));
+    await tester
+        .pumpAndSettle(); // espera todos os microServiços que alteram a tela acabarem
+    expect(find.text('\$10.0'), findsOneWidget);
+  });
 }
